c96ca74cbea5f387885672ee5d085a1ad8f5e0b2
aboody-12 <147659043+aboody-12@users.noreply.github.com>
2023-10-14 03:38:26 -0700

    Add files via upload

---
+++ codeline_task-main/README.md
@@ -0,0 +1,6 @@
+# codeline_task
+solution of tasks
+
+Name:Abdullah Ali Khalid AL-Harthi
+Email:abdullaali2390@gmail.com
+phone:+968 97802390 
---
+++ codeline_task-main/bonustask.py
@@ -0,0 +1,43 @@
+1 -- Print Pattern
+2 -- Rotate Array 
+3 --  Help
+4 -- Exit
+Enter your choice: 1
+Enter the number of rows for the pattern: 5
+* * * * * 
+* * * * 
+* * * 
+* * 
+* 
+1 -- Print Pattern
+2 -- Rotate Array 
+3 --  Help
+4 -- Exit
+Enter your choice: 2
+Enter the size of the array: 7
+Enter the number of rotations: 3
+Enter the array elements: 1 2 3 4 5 6 7
+Rotated array:  [5, 6, 7, 1, 2, 3, 4]
+1 -- Print Pattern
+2 -- Rotate Array 
+3 --  Help
+4 -- Exit
+Enter your choice: 3
+--Help-- 
+           Option 1: Print a  pattern with 'n' rows of decreasing asterisks.
+            Option 2: Rotate an array of  'n' elements of the right by  'k' steps.
+            Option 3: Display this help message.
+            Option 4: Exit the program.
+        
+1 -- Print Pattern
+2 -- Rotate Array 
+3 --  Help
+4 -- Exit
+Enter your choice: 4
+Exiting the program. Goodbye!
+Traceback (most recent call last):
+  File "<string>", line 67, in <module>
+  File "<string>", line 49, in option4
+  File "<frozen sitebuiltins>", line 26, in __call_
+SystemExit: None
+>
